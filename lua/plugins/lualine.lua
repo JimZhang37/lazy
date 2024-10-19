ce94253327b79a7f9246364fd4a3162385d80b0c
Yaohua <zhangyaohua40605@gmail.com>
2024-10-19 18:59:33 +0800

    Lualine can show the macro recording when i use "q" to record a macro, the status bar didn't show anything.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,54 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {}
+  config = function()
+    local function macro_recording()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == '' then
+        return ''
+      else
+        return 'Recording @' .. recording_register
+      end
+    end
+
+
+
+
+
+
+    local function lsp_status()
+      local clients = vim.lsp.get_active_clients()
+      if next(clients) == nil then
+        return 'No LSP'
+      end
+      local names = {}
+      for _, client in ipairs(clients) do
+        table.insert(names, client.name)
+      end
+      return table.concat(names, ', ')
+    end
+
+    require('lualine').setup {
+      options = {
+        theme = 'auto',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location', macro_recording, lsp_status }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
+    }
+  end
 }
